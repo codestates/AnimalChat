aa458d3fd8ff48aa7bb10f7ca6060495c071b750
kingshuny <nicecap77@gmail.com>
2021-10-10 11:51:34 +0000

    가지고있는거 모두 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -7,44 +7,26 @@ import Post from "./pages/6.post"
 import "./App.css"
 
 function App() {
-  // /// 서버배포정상 완료시 테스트 코드
   const [isLogin, setIsLogin] = useState(false)
-  function onclickButton() {
-    axios
-      .get("http://ec2-3-34-2-204.ap-northeast-2.compute.amazonaws.com", {
-        withCredentials: true,
-      })
-      .then(function (response) {
-        setIsLogin(true)
-      })
-  }
+
   return (
-    <div className="App">
-      <button onClick={onclickButton}> 버튼 </button>
-      {isLogin ? "hello world" : "welcome hell world :)"}
+    <div>
+      <Switch>
+        <Route path="/firstpage">
+          <FirstPage />
+        </Route>
+        <Route exact path="/mainpage">
+          <MainPage />
+        </Route>
+        <Route exact path="/post">
+          <Post />
+        </Route>
+        <Route path="/">
+          {isLogin ? <Redirect to="/mainpage" /> : <Redirect to="/firstpage" />}
+        </Route>
+      </Switch>
     </div>
   )
-  // ///
-
-  // const [isLogin, setIsLogin] = useState(false)
-  // return (
-  //   <div>
-  //     <Switch>
-  //       <Route path="/firstpage">
-  //         <FirstPage />
-  //       </Route>
-  //       <Route exact path="/mainpage">
-  //         <MainPage />
-  //       </Route>
-  //       <Route exact path="/post">
-  //         <Post />
-  //       </Route>
-  //       <Route path="/">
-  //         {isLogin ? <Redirect to="/mainpage" /> : <Redirect to="/firstpage" />}
-  //       </Route>
-  //     </Switch>
-  //   </div>
-  // )
 }
 
 export default App
